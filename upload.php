bc8ed410ed6d175782602431de8c4613a1ea4c73
srinjoychinargoswami <66094180+srinjoychinargoswami@users.noreply.github.com>
2021-01-14 13:26:38 -0500

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,30 @@
+<?php
+$target_dir = "/HCBiologysyllabusbank";
+$target_file = /HCBiologysyllabusbank . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 50000000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "pdf" && $imageFileType != "docx" && $imageFileType != "doc" ) {
+  echo "Sorry, only pdf, docx, doc files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
